4b537c0fbc43dadeb8e51be72502f0e32aa57b16
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-05-06 16:51:19 +0100

    Removed API key

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
-    Authorization: 'Bearer DcU25Q1PL9oksJpIGT13BIt3liQGv1urRHqRkWHh0ZlUiMToRCrVRplZIHyJ_LNmBeUr-90xSLKkQbwsbo75W_wwjhM_bq-dyv7juNBskBg0kwCuhob5vS2J8suKYHYx'
+    Authorization: 'Bearer INSERT_YELP_API_KEY'
   }
 });
